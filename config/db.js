472e127072d04d8febdefb1125560b12ac77ae9d
206620219 <Brian.DeMaio@universalorlando.com>
2020-01-03 12:27:00 -0500

    Setting up connection to mongodb

---
+++ config/db.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true
+    });
+  } catch (error) {
+    console.log("The error to the connect db was " + error);
+  }
+};
